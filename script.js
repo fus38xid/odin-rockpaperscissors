e8e4f2017a1bfedf9b42da521fdc5e59ce473269
fus38xid <fus38xid@uni-duesseldorf.de>
2023-08-28 20:47:12 +0200

    Added playerSelection function non case sensitivity by forcing toUpperCase

--- script.js
+++ script.js
@@ -7,6 +7,41 @@ function getComputerChoice (){
     return (PICK[random]);
 
 }
+/*
 getComputerChoice();
 
 console.log(getComputerChoice());
+*/
+
+function playerSelection() {
+    //prompt user for their choice
+    //return the choice
+    let playerChoice = prompt("Rock, Paper, or Scissors?");
+    playerChoice = playerChoice.toUpperCase();
+    return playerChoice;
+}
+
+//playerSelection(); doesnt work with the playround function must be called within the playRound
+
+function playRound(playerSelection, getComputerChoice) {
+    //compare playerSelection to getComputerChoice
+    //return the outcome
+    if (playerSelection === "ROCK" && getComputerChoice === "Scissors") {
+        return "You Win! Rock beats Scissors";
+    } else if (playerSelection === "ROCK" && getComputerChoice === "Paper") {
+        return "You Lose! Paper beats Rock";
+    } else if (playerSelection === "PAPER" && getComputerChoice === "Rock") {
+        return "You Win! Paper beats Rock";
+    } else if (playerSelection === "PAPER" && getComputerChoice === "Scissors") {
+        return "You Lose! Scissors beats Paper";
+    } else if (playerSelection === "SCISSORS" && getComputerChoice === "Paper") {
+        return "You Win! Scissors beats Paper";
+    } else if (playerSelection === "SCISSORS" && getComputerChoice === "Rock") {
+        return "You Lose! Rock beats Scissors";
+    } else {
+        return "It's a tie!";
+    }
+    
+}
+
+console.log(playRound(playerSelection(), getComputerChoice()));
