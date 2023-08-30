6aec2861a80aa8611d94d94cbac627ef337bc729
fus38xid <fus38xid@uni-duesseldorf.de>
2023-08-30 19:50:19 +0200

    added Reminder and TODO list into the script.js file

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+/*
+REMINDER: to push changes use: git push origin rps-ui
+
+
+TODO:
+
+1.    For now, remove the logic that plays exactly five rounds.
+2.    Create three buttons, one for each selection. Add an event listener to the buttons that call your playRound function with the correct playerSelection every time a button is clicked. (you can keep the console.logs for this step)
+3.    Add a div for displaying results and change all of your console.logs into DOM methods.
+4.    Display the running score, and announce a winner of the game once one player reaches 5 points.
+5.    You will likely have to refactor (rework/rewrite) your original code to make it work for this. That’s OK! Reworking old code is an important part of a programmer’s life.
+
+
+*/
+
+
+
+
 function getComputerChoice (){
     //select Rock Paper or Scissors at random
     //return the choice
