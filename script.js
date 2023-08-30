e0d4422fb34493e520fb86d52e1372c0a95c7a0b
fus38xid <fus38xid@uni-duesseldorf.de>
2023-08-30 19:57:55 +0200

    Added Done section in script.js and went ahead with completion of the first todo (remove logic that plays five rounds(commented out the for loop))

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 /*
 REMINDER: to push changes use: git push origin rps-ui
-
+   
 
 TODO:
 
-1.    For now, remove the logic that plays exactly five rounds.
+
 2.    Create three buttons, one for each selection. Add an event listener to the buttons that call your playRound function with the correct playerSelection every time a button is clicked. (you can keep the console.logs for this step)
 3.    Add a div for displaying results and change all of your console.logs into DOM methods.
 4.    Display the running score, and announce a winner of the game once one player reaches 5 points.
 5.    You will likely have to refactor (rework/rewrite) your original code to make it work for this. That’s OK! Reworking old code is an important part of a programmer’s life.
+
+DONE:
+    ✅ 1.    For now, remove the logic that plays exactly five rounds.
+    
 
 
 */
@@ -51,11 +55,11 @@ function game() {
     let SCORES_HUMAN = 0;
     let SCORES_COMPUTER = 0;
 
-    for(let i = 0; i<5; i++) {
+    //for(let i = 0; i<5; i++) {
         playRound();
         console.log(playRound(playerSelection(), getComputerChoice()));
         
-    }
+    //}
 
     if(SCORES_HUMAN > SCORES_COMPUTER) {
         console.log("You Win!");
